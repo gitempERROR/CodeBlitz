7c7765fc71e12f24c9c0c7bb6e97ebf0bb52ac87
tmpERROR <errorfadeev@yandex.ru>
2024-11-08 14:35:06 +0300

    feat: create project, define structure, add theme

---
+++ app/src/main/java/com/example/codeblitz/view/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.codeblitz.view.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Background = Color(0xff28242D)
+val Primary = Color(0xffDF3E78)
+val SecondaryContainer = Color(0xffD3CADC)
+val Tertiary = Color(0xffFFFFFF)
+val Secondary = Color(0xff62E3FF)
+val OnBackground = Color(0xff3F3343)
---
+++ app/src/main/java/com/example/codeblitz/view/ui/theme/Theme.kt
@@ -0,0 +1,44 @@
+package com.example.codeblitz.view.ui.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.material3.Typography
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Primary,
+    secondary = Secondary,
+    tertiary = Tertiary,
+    background = Background,
+    onBackground = OnBackground,
+    secondaryContainer = SecondaryContainer
+)
+
+val LocalColors = staticCompositionLocalOf { DarkColorScheme }
+val LocalTypography = staticCompositionLocalOf { Typography }
+
+@Composable
+fun CodeBlitzTheme(
+    typography: Typography = CodeBlitzTheme.typography,
+    content: @Composable () -> Unit
+) {
+    val colors = DarkColorScheme
+
+    CompositionLocalProvider(
+        LocalColors provides colors,
+        LocalTypography provides typography,
+        content = content
+    )
+}
+
+object CodeBlitzTheme {
+    val colors : ColorScheme
+        @Composable @ReadOnlyComposable
+        get() = LocalColors.current
+    val typography : Typography
+        @Composable @ReadOnlyComposable
+        get() = LocalTypography.current
+}
